8afe533c14d5d8b58a934c02735bde38d4cc169f
AudiensK <317622658@qq.com>
2025-05-16 21:22:18 +0800

    daily study Cl 5-16-eve

--- CL/struct.c
+++ CL/struct.c
@@ -11,6 +11,8 @@ struct Student {
 int main() {
     // 声明一个学生结构体变量
     struct Student s1;
+    // 声明一个学生结构体变量并初始化
+    struct Student s2 = {.name = {"Jack"}, .age = 18};
 
     // 初始化结构体变量
     strcpy(s1.name, "John");
@@ -21,6 +23,11 @@ int main() {
     printf("Name: %s\n", s1.name);
     printf("Age: %d\n", s1.age);
     printf("Score: %.2f\n", s1.score);
+
+    printf("\n");
+    printf("Name: %s\n", s2.name);
+    printf("Age: %d\n", s2.age);
+    printf("Score: %.2f\n", s2.score);
 
     return 0;
 }
